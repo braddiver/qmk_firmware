bdfad54858559a48f7888772dce7f67117c4c5b6
Brad Diver <brad_diver@comcast.com>
2022-03-27 15:58:51 -0400

    crkbd layout comments

--- keyboards/crkbd/keymaps/braddiver/keymap.c
+++ keyboards/crkbd/keymaps/braddiver/keymap.c
@@ -42,19 +42,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB, MEH_T(KC_SPC), TG(4)
   ),
 
+  // Symbol layer for left hand. Right holds pinky middle.
   [1] = LAYOUT_split_3x5_3(
     // -- Left (top)
-    KC_EXLM, KC_AT, KC_LCBR, KC_RCBR, KC_NO,
+    KC_EXLM, KC_AT, KC_LCBR, KC_RCBR, KC_NO, // ! @ { } <None>
     // -- Right (top)
     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
 
     // -- Left (middle)
-    KC_T, KC_EQL, KC_LPRN, KC_RPRN, KC_HASH,
+    KC_T, KC_EQL, KC_LPRN, KC_RPRN, KC_HASH, // t = ( ) #
     // -- Right (middle)
     KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS,
 
     // -- Left (bottom)
-    KC_CIRC, KC_PERC, KC_LBRC, KC_RBRC, KC_TILD,
+    KC_CIRC, KC_PERC, KC_LBRC, KC_RBRC, KC_TILD, // ^ % [ ] ~
     // -- Right (bottom)
     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
 
@@ -64,44 +65,53 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRNS, KC_TRNS, KC_TRNS
   ),
 
+  // Symbol layer for right hand. Left holds pinky or middle finger.
   [2] = LAYOUT_split_3x5_3(
     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-    KC_NO, KC_PAST, KC_PLUS, KC_DQUO, KC_UNDS,
+    KC_NO, KC_PAST, KC_PLUS, KC_DQUO, KC_UNDS, // <None> * + " _
 
     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-    KC_PMNS, KC_QUOT, KC_AMPR, KC_BSLS, KC_ENT,
+    KC_PMNS, KC_QUOT, KC_AMPR, KC_BSLS, KC_ENT, // - ' & \ <Enter>
 
     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-    KC_NO, KC_Y, KC_PIPE, KC_GRV, KC_DLR,
+    KC_NO, KC_Y, KC_PIPE, KC_GRV, KC_DLR, // <None> y | ` $
 
     KC_TRNS, KC_TRNS, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS
   ),
 
+  // Number layer for right hand. Left hand holds index finger home row.
   [3] = LAYOUT_split_3x5_3(
     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-    KC_PLUS, KC_7, KC_8, KC_9, KC_ASTR,
+    KC_PLUS, KC_7, KC_8, KC_9, KC_ASTR, // + 7 8 9 *
 
-    TO(0), KC_NO, KC_NO, KC_NO, KC_NO,
-    KC_DOT, KC_4, KC_5, KC_6, KC_0,
+    TO(0), KC_NO, KC_NO, KC_NO, KC_NO, // <Layer0>
+    KC_DOT, KC_4, KC_5, KC_6, KC_0, // . 4 5 6 0
 
     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-    KC_EQL, KC_1, KC_2, KC_3, KC_HASH,
+    KC_EQL, KC_1, KC_2, KC_3, KC_HASH, // = 1 2 3 #
 
     KC_TRNS, KC_TRNS, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS
   ),
 
   [4] = LAYOUT_split_3x5_3(
+    // <ForceQuit> <CloseWindow> <PrevTab> <NextTab> <None>
     LALT(LGUI(KC_ESCAPE)), LGUI(KC_W), LCTL(RSFT(KC_TAB)), LCTL(KC_TAB), KC_NO,
+    // <None> <TextSmaller> <TextBigger> <TextNormalSize> <None>
     KC_NO, LGUI(KC_PMNS), LGUI(KC_EQL), LGUI(KC_0), KC_NO,
 
+    // <Layer0> <Esc> <PrevApp> <NextApp> <ScreenShotRectangle>
     TO(0), KC_ESC, LSFT(KC_TAB), LGUI(KC_TAB), LGUI(LSFT(KC_4)),
+    // <LeftArrow> <DownArrow> <UpArrow> <RightArrow> <Enter>
     KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_ENT,
 
+    // <None> <PrevApp(w/ThumbCommand)> <NextApp(w/ThumbCommand)> <None>
     KC_NO, LSFT(KC_TAB), KC_TAB, KC_LCTL, KC_NO,
+    // <None> <PrevBrowseHistory> <NextBrowseHistory> <Transparent> <...>
     KC_NO, LGUI(KC_LBRC), LGUI(KC_RBRC), KC_TRNS, KC_TRNS,
 
+    // <Layer3> <Transparent> <...>
     TO(3), KC_TRNS, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS
   )
