cdccd420359e909d3c01aa30da0f3ae5f894654b
Brad Diver <brad_diver@comcast.com>
2022-04-06 13:57:04 -0400

    fix: Typing (MT(LSHFT, C)) 'ck' fast outputs 'K'.

--- keyboards/crkbd/keymaps/braddiver/config.h
+++ keyboards/crkbd/keymaps/braddiver/config.h
@@ -4,3 +4,7 @@
 #define QMK_BUILDDATE "2022-04-03-16:22:11"
 #define CHIBIOS_VERSION "develop_2021_q4"
 #define CHIBIOS_CONTRIB_VERSION "develop_2021_q3-8-gd1c212"
+// Wait before triggering mod version of mod-tap keys
+#define TAPPING_TERM 200
+// Respect above term for shift and command. Ex. with LSFT_T(KC_C) typing 'ck' fast will output 'K'.
+#define IGNORE_MOD_TAP_INTERRUPT
