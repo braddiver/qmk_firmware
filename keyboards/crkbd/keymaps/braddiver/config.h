0da05190688478ac442be830870a1c23d8a30263
braddiver <braddiver@gmail.com>
2024-12-27 14:33:44 -0500

    restore quick/slow mod key behavior

--- keyboards/crkbd/keymaps/braddiver/config.h
+++ keyboards/crkbd/keymaps/braddiver/config.h
@@ -2,3 +2,5 @@
 
 // Wait before triggering mod version of mod-tap keys
 #define TAPPING_TERM 200
+// But define which mod keys need to wait (like Shift). For others, quick typing uses the mod (like App switching with Cmd-TAB).
+#define HOLD_ON_OTHER_KEY_PRESS_PER_KEY
